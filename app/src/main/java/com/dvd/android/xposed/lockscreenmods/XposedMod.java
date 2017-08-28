e6b131772b6f20724ffecf8bec1fb64a70f1f095
dvdandroid <davide.intelligent@gmail.com>
2017-08-28 12:06:21 +0200

    Avoid SecurityException on Android 23+

--- app/src/main/java/com/dvd/android/xposed/lockscreenmods/XposedMod.java
+++ app/src/main/java/com/dvd/android/xposed/lockscreenmods/XposedMod.java
@@ -1,9 +1,14 @@
 package com.dvd.android.xposed.lockscreenmods;
 
+import android.os.Build;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class XposedMod implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 
@@ -12,6 +17,15 @@ public class XposedMod implements IXposedHookZygoteInit, IXposedHookLoadPackage 
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
+        if (Build.VERSION.SDK_INT > 23) { // for context.getSharedPreferences("", MODE_WORLD_READABLE) disabled from nougat.
+            findAndHookMethod("android.app.ContextImpl", null, "checkMode", int.class, new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                    return null;
+                }
+            });
+        }
+
         prefs = new XSharedPreferences(PACKAGE_NAME);
         prefs.makeWorldReadable();
     }
